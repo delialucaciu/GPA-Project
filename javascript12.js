94951d262fa3468079b101c202fc773de6f2552d
Delia Lucaciu <delialucaciu19@gmail.com>
2022-01-31 20:13:41 +0200

    first commit

---
+++ javascript12.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+ let x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+
